4ce1b39282544043affb6379f156d5a903194ac1
JMteam09 <JMgamerZzz@gmail.com>
2017-01-19 13:19:20 +0100

    trial van de custom dialog toegevoegd

--- app/src/main/java/com/estimote/notification/MainActivity.java
+++ app/src/main/java/com/estimote/notification/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ME = this;
+        util.showdialog(this,"test","close");
     }
 
     @Override
---
+++ app/src/main/java/com/estimote/notification/util.java
@@ -0,0 +1,48 @@
+package com.estimote.notification;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by reloa on 19-1-2017.
+ */
+
+public class util {
+    public static void showdialog(Activity target, String text, String button){
+        showdialog(target,text,button,null);
+    }
+    public static void showdialog(Activity target, String text, String button, Drawable background){
+        final Dialog d = new Dialog(target);
+        d.setCancelable(false);
+        d.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        d.setContentView(R.layout.dialog);
+        d.show();
+        Button b = (Button) d.findViewById(R.id.Dialog_button);
+        TextView message = (TextView) d.findViewById(R.id.Dialog_Text);
+        ImageView img = (ImageView) d.findViewById(R.id.Dialog_Image);
+        if (background != null) {
+            img.setImageDrawable(background);
+        }
+        b.setText(button);
+        message.setText(text);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                d.setCancelable(true);
+                d.cancel();
+            }
+        });
+    }
+}
