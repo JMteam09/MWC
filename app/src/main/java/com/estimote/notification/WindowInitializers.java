781708df4245f4e84e971ad0469c7adacbb86ffd
Xyclus <xyclusnl@gmail.com>
2017-02-17 15:59:55 +0100

    Programma erin gezet

--- app/src/main/java/com/estimote/notification/WindowInitializers.java
+++ app/src/main/java/com/estimote/notification/WindowInitializers.java
@@ -10,10 +10,17 @@ public class WindowInitializers {
             public void run() {
                 MainActivity.ME.setContentView(R.layout.activity_main);
                 Button buttonView_route = (Button) MainActivity.ME.findViewById(R.id.buttonView_route);
+                Button buttonView_proef = (Button) MainActivity.ME.findViewById(R.id.buttonView_programma) ;
                 buttonView_route.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         WindowInitializer in = WindowInitializers.RouteActivity(MainActivity.current).Open();
+                    }
+                });
+                buttonView_proef.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void  onClick(View v) {
+                        WindowInitializer in = WindowInitializers.ProefActivity(MainActivity.current).Open();
                     }
                 });
             }
@@ -26,6 +33,16 @@ public class WindowInitializers {
             @Override
             public void run() {
                 MainActivity.ME.setContentView(R.layout.activity_route);
+            }
+        });
+        return window;
+    }
+
+    public static WindowInitializer ProefActivity(WindowInitializer prev) {
+        WindowInitializer window = new WindowInitializer(new Runnable() {
+            @Override
+            public void run() {
+                MainActivity.ME.setContentView(R.layout.activity_proeflessen);
             }
         });
         return window;
